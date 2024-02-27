49fe65c224928699ee8a72c5277d3f9726b311b5
Carlos Sousa <carlosmanuel.desousa@digitalcareerinstitute.org>
2024-02-27 20:36:31 +0100

    add current weather dinamic, fetch api forecast and weather

--- src/components/search/Api.js
+++ src/components/search/Api.js
@@ -1,11 +1,13 @@
-
- export const geoApiOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '776c525bbamshd95d2e2b84dd390p1cc282jsn503c17b0e411',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "776c525bbamshd95d2e2b84dd390p1cc282jsn503c17b0e411",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
 };
 
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+export const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5`;
+
+export const WEATHER_API_KEY = `b1ec26b7b59cf753655b8afd5a0a854f`;
